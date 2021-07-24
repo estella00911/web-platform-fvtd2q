948238d6acebb59364419febe4aa27c4b3395743
estella00911 <estella00911@gmail.com>
2021-07-24 12:15:56 +0800

    1 file modified

--- p2/p2.js
+++ p2/p2.js
@@ -1,13 +1,13 @@
-document.querySelector(".form").addEventListener('click', function (e) {
+document.querySelector('.form').addEventListener('click', function(e) {
   let target = e.target;
   let inputText = document.querySelector('.form__input').value;
-  if (target.classList.contains("btn-add")) {
-    let newList = document.createElement("li");
+  if (target.classList.contains('btn-add')) {
+    let newList = document.createElement('li');
     newList.innerHTML = `${inputText}<button class="btn-delete">刪除</button>`;
     document.querySelector('.lists').appendChild(newList);
-  } else if (target.classList.contains("btn-delete")) {
-    let deleteLi = target.parentNode
-    target.parentNode.parentNode.removeChild(deleteLi)
-  } else if (target.classList.contains("btn-delete-all")) {
-
-  })
+  } else if (target.classList.contains('btn-delete')) {
+    let deleteLi = target.parentNode;
+    target.parentNode.parentNode.removeChild(deleteLi);
+  } else if (target.classList.contains('btn-delete-all')) {
+  }
+});
