b9226096423e82996009c635fb0ae0db2bde7a58
estella00911 <estella00911@gmail.com>
2021-07-24 13:53:10 +0800

    補上 p4 code，但 console.log 結果與影片不太一樣

--- p4/p4.js
+++ p4/p4.js
@@ -1,5 +1,33 @@
 // 請把解題函式寫在這個裡面
 function solve(arr) {
+  // arr[0].value: id=1 值 43
+  // arr[0].id: id=1
+
+  var allId = arr.map(function(item) {
+    return item.id;
+  });
+
+  var NotRepeatingId = allId.filter(function(item, index, array) {
+    return array.indexOf(item) === index;
+  });
+  // console.log(NotRepeatingId);
+
+  for (let j = 0; j < NotRepeatingId.length; j++) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id == NotRepeatingId[j]) {
+        console.log('arr[i]', arr[i].id);
+        console.log('NotRepeatingId'[j], NotRepeatingId[j]);
+        let val = arr[i].value;
+        array = newArray.push(val);
+      }
+    }
+    console.log(newArray);
+  }
+  var allNum = arr.map(function(item) {
+    return item.value;
+  });
+  // console.log(allNum);
   return [];
 }
 
